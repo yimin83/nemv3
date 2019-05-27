3d08c3d4f421d54b8a7f688358ed7e751d64b199
yimin83 <yimin83@gmail.com>
2019-05-27 14:50:04 +0900

    20190527 #2 토글 수정

--- fe/src/views/About.vue
+++ fe/src/views/About.vue
@@ -27,7 +27,7 @@
             <v-btn flat color="purple">Explore</v-btn>
             <v-spacer></v-spacer>
             <v-btn icon @click="show = !show"><!-- 토글처리-->
-              <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+              <v-icon>{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down'  }}</v-icon>
             </v-btn>
           </v-card-actions>
 
